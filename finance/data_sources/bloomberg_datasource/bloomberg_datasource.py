d7d835ea5be7be1112c4b0f9dd5edb11b2171648
Bastian <Tim.Bastian@tcw.com>
2023-06-20 10:21:26 -0700

    add untracked files

---
+++ finance/data_sources/bloomberg_datasource/bloomberg_datasource.py
@@ -0,0 +1,47 @@
+import quantkit.finance.data_sources.data_sources as ds
+import quantkit.utils.logging as logging
+import pandas as pd
+
+
+class BloombergDataSource(ds.DataSources):
+    """
+    Provide information on company level from Bloomberg
+
+    Parameters
+    ----------
+    params: dict
+        datasource specific parameters including source
+
+    Returns
+    -------
+    DataFrame
+    """
+
+    def __init__(self, params: dict):
+        super().__init__(params)
+
+    def load(self):
+        """
+        load data and transform dataframe
+        """
+        logging.log("Loading Bloomberg Data")
+        self.datasource.load()
+        self.transform_df()
+        return
+
+    def transform_df(self):
+        """
+        None
+        """
+
+        return
+
+    @property
+    def df(self):
+        """
+        Returns
+        -------
+        DataFrame
+            df
+        """
+        return self.datasource.df
---
+++ finance/data_sources/msci_datasource/msci_datasource.py
@@ -0,0 +1,56 @@
+import quantkit.finance.data_sources.data_sources as ds
+import quantkit.utils.logging as logging
+import pandas as pd
+
+
+class MSCIDataSource(ds.DataSources):
+    """
+    Provide information on company level from MSCI
+
+    Parameters
+    ----------
+    params: dict
+        datasource specific parameters including source
+
+    Returns
+    -------
+    DataFrame
+    """
+
+    def __init__(self, params: dict):
+        super().__init__(params)
+
+    def load(self):
+        """
+        load data and transform dataframe
+        """
+        logging.log("Loading MSCI Data")
+        self.datasource.load()
+        self.transform_df()
+        return
+
+    def transform_df(self):
+        """
+        - fill GICS na's with 'Unassigned GICS'
+        - change values for columns in params["transformation"]
+        - add row for NoISIN
+        """
+        # fill GICS industry NA's with 'Unassigned GICS'
+        self.datasource.df["GICS_SUB_IND"] = self.datasource.df["GICS_SUB_IND"].fillna(
+            "Unassigned GICS"
+        )
+
+        # replace values in each column from params transformation file
+        self.datasource.df = self.datasource.df.replace(self.params["transformation"])
+
+        return
+
+    @property
+    def df(self):
+        """
+        Returns
+        -------
+        DataFrame
+            df
+        """
+        return self.datasource.df
