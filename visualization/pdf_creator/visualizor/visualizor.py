42de96835b6747cdb7c853ee1ad374f453517701
Bastian <Tim.Bastian@tcw.com>
2023-07-28 14:20:26 -0700

    left column

--- visualization/pdf_creator/visualizor/visualizor.py
+++ visualization/pdf_creator/visualizor/visualizor.py
@@ -12,8 +12,47 @@ PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("../data").resolve()
 
 
-df_fund_facts = pd.read_csv(DATA_PATH.joinpath("df_fund_facts.csv"))
 df_price_perf = pd.read_csv(DATA_PATH.joinpath("df_price_perf.csv"))
+
+df_WACI = pd.DataFrame(
+    data={"Name": ["Portfolio", "Index", "Carbon Reduction"], "Value": [50, 100, -50]}
+)
+df_risk_score_distr = pd.DataFrame(
+    data={
+        "": ["E&S", "Governance", "Transition", "Overall"],
+        "Portfolio": [2, 2, 2, 2],
+        "Index": [3, 3, 3, 3],
+    }
+)
+df_distr = pd.DataFrame(
+    data={
+        "Name": [
+            "Leading ESG Score",
+            "Average ESG Score",
+            "Poor Risk Score",
+            "Not Scored",
+            "Total",
+        ],
+        "Value": [30, 50, 10, 10, 100],
+    }
+)
+df_ci_sector = pd.DataFrame(
+    data={
+        "Name": [
+            "Utilities",
+            "Unassigned GICS",
+            "Energy",
+            "Materials",
+            "Industrials",
+            "Real Estate",
+            "Consumer Staples",
+            "Consumer Discretionary",
+            "Communication Services",
+            "Information Technology",
+        ],
+        "Value": [1706, 1219, 527, 250, 224, 70, 36, 31, 27, 19],
+    }
+)
 
 
 class Visualizor(object):
@@ -63,18 +102,193 @@ class Visualizor(object):
         )
         return header
 
+    def add_bar_chart(self) -> dcc.Graph:
+        bar = dcc.Graph(
+            figure={
+                "data": [
+                    go.Bar(
+                        x=[
+                            "2008",
+                            "2009",
+                            "2010",
+                            "2011",
+                            "2012",
+                            "2013",
+                            "2014",
+                            "2015",
+                            "2016",
+                            "2017",
+                            "2018",
+                        ],
+                        y=[
+                            "10000",
+                            "7500",
+                            "9000",
+                            "10000",
+                            "10500",
+                            "11000",
+                            "14000",
+                            "18000",
+                            "19000",
+                            "20500",
+                            "24000",
+                        ],
+                    )
+                ],
+                "layout": go.Layout(
+                    autosize=True,
+                    title="",
+                    font={"family": "Raleway", "size": 10},
+                    height=200,
+                    width=400,
+                    hovermode="closest",
+                    legend={
+                        "x": -0.0277108433735,
+                        "y": -0.142606516291,
+                        "orientation": "h",
+                    },
+                    margin={
+                        "r": 20,
+                        "t": 20,
+                        "b": 20,
+                        "l": 50,
+                    },
+                    showlegend=True,
+                    xaxis={
+                        "autorange": True,
+                        "linecolor": "rgb(0, 0, 0)",
+                        "linewidth": 1,
+                        "range": [2008, 2018],
+                        "showgrid": False,
+                        "showline": True,
+                        "title": "",
+                        "type": "linear",
+                    },
+                    yaxis={
+                        "autorange": False,
+                        "gridcolor": "rgba(127, 127, 127, 0.2)",
+                        "mirror": False,
+                        "nticks": 4,
+                        "range": [0, 30000],
+                        "showgrid": True,
+                        "showline": True,
+                        "ticklen": 10,
+                        "ticks": "outside",
+                        "title": "$",
+                        "type": "linear",
+                        "zeroline": False,
+                        "zerolinewidth": 4,
+                    },
+                ),
+            },
+            config={"displayModeBar": False},
+        )
+        return bar
+
+    def add_table(self, df, multi_column=False, header=False) -> html.Table:
+        """Return a dash definition of an HTML table for a Pandas dataframe"""
+        table = []
+        if header:
+            header_row = []
+            for i, col in enumerate(df.columns):
+                if i == 0:
+                    header_row.append(html.Th(col))
+                else:
+                    header_row.append(html.Th(col, className="vertical"))
+            table.append(html.Tr(header_row, className="table-header"))
+
+        if multi_column:
+            class_td = "table-element vertical"
+            class_table = "multi-table"
+        else:
+            class_td = "table-element"
+            class_table = ""
+
+        for index, row in df.iterrows():
+            html_row = []
+            for i in range(len(row)):
+                if i == 0:
+                    html_row.append(html.Td([row[i]]))
+                else:
+                    html_row.append(html.Td([row[i]], className=class_td))
+            table.append(html.Tr(html_row))
+        return html.Table(table, className=class_table)
+
+    def add_WACI_table(self) -> html.Div:
+        waci = html.Div(
+            [
+                html.H6(
+                    [
+                        "Weighted Average Carbon Intensity - ",
+                        html.Br(),
+                        "Tons CO",
+                        html.Sub(2),
+                        "e/$M Sales",
+                        html.Sup(1, className="superscript"),
+                    ],
+                    className="subtitle padded",
+                ),
+                self.add_table(df_WACI),
+            ],
+            className="row",
+        )
+        return waci
+
+    def add_risk_score_distribution(self) -> html.Div:
+        distr = html.Div(
+            [
+                html.H6(
+                    [
+                        "TCW ESG Risk Score Distribution",
+                        html.Sup(2, className="superscript"),
+                    ],
+                    className="subtitle padded",
+                ),
+                self.add_table(df_risk_score_distr, multi_column=True, header=True),
+            ],
+            className="row",
+        )
+        return distr
+
+    def add_score_distribution(self) -> html.Div:
+        distr = html.Div(
+            [
+                html.H6(
+                    [
+                        "TCW Score Distribution (% MV)",
+                        html.Sup(3, className="superscript"),
+                    ],
+                    className="subtitle padded",
+                ),
+                self.add_table(df_distr),
+            ],
+            className="row",
+        )
+        return distr
+
+    def add_carbon_intensity_chart(self) -> html.Div:
+        ci_chart = html.Div(
+            [
+                html.H6(
+                    "Carbon Intensity by Sector",
+                    className="subtitle padded",
+                ),
+                self.add_bar_chart(),
+            ],
+            className="row",
+        )
+        print(ci_chart)
+        return ci_chart
+
     def add_equity_body(self) -> html.Div:
         body = html.Div(
             [
+                # first columns
                 html.Div(
                     [
-                        html.Div(
-                            [
-                                html.H6(["Fund Facts"], className="subtitle padded"),
-                                html.Table(make_dash_table(df_fund_facts)),
-                            ],
-                            className="row",
-                        ),
+                        self.add_WACI_table(),
+                        self.add_risk_score_distribution(),
+                        self.add_score_distribution(),
                     ],
                     className="three columns",
                 ),
@@ -177,22 +391,11 @@ class Visualizor(object):
                     className="five columns",
                 ),
                 html.Div(
-                    [
-                        html.Div(
-                            [
-                                html.H6(
-                                    "Price & Performance (%)",
-                                    className="subtitle padded",
-                                ),
-                                html.Table(make_dash_table(df_price_perf)),
-                            ],
-                            className="row",
-                        ),
-                    ],
+                    [self.add_carbon_intensity_chart()],
                     className="four columns",
                 ),
             ],
-            className="sub_page",
+            className="sub_page twelve columns",
         )
         return body
 
