b681c6efb6acc1f9f565922b1f1dd08800924949
Bastian <Tim.Bastian@tcw.com>
2023-09-08 14:27:28 -0700

    viz folder

---
+++ visualization/asset_allocation/returns.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from matplotlib.ticker import PercentFormatter
+import pandas as pd
+
+
+def strategy_returns(strategy_df: pd.DataFrame) -> None:
+    """
+    For a given DataFrame of returns, plot the line chart of cumulative returns
+
+    Parameters
+    ----------
+    strategy_df: pd.DataFrame
+        DataFrame with portfolio_name and daily returns
+    """
+    strategy_df = strategy_df.dropna()
+    strategy_df["return"] = strategy_df["return"] + 1
+
+    grouped = strategy_df.groupby("portfolio_name")
+
+    fig, ax = plt.subplots(figsize=(8, 6))
+    for name, group in grouped:
+        if name == "ex_ante_mean_variance":
+            continue
+        grouped_df = group["return"].cumprod()
+        grouped_df.plot(ax=ax, label=name)
+
+    ax.yaxis.set_major_formatter(PercentFormatter(1, decimals=0))
+    plt.legend()
