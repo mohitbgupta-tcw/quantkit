b27c4660d873dd5b4dfd1f94c6d518eb9ed3956f
Bastian <Tim.Bastian@tcw.com>
2023-10-06 13:17:46 -0700

    running sdg query

--- finance/data_sources/sdg_datasource/sdg_datasource.py
+++ finance/data_sources/sdg_datasource/sdg_datasource.py
@@ -5,10 +5,10 @@ import pandas as pd
 from copy import deepcopy
 
 
-class SDGDataSource(object):
+class SDGDataSource(ds.DataSources):
     """
     Provide SDG data (sustainability measures) on company level
-    Pull data from SDG and SDGA data source
+    Pull data from ISS
 
     Parameters
     ----------
@@ -25,8 +25,7 @@ class SDGDataSource(object):
     """
 
     def __init__(self, params: dict, **kwargs) -> None:
-        self.sdg_datasource = SDGData(params["sdg"], **kwargs)
-        self.sdga_datasource = SDGData(params["sdga"], **kwargs)
+        super().__init__(params, **kwargs)
         self.sdg = dict()
 
     def load(self) -> None:
@@ -34,32 +33,65 @@ class SDGDataSource(object):
         load data and transform dataframe
         """
         logging.log("Loading SDG Data")
-        self.sdg_datasource.load()
-        self.sdga_datasource.load()
+        query = f"""
+        SELECT 
+            sdga.issuer_id, 
+            sdga.issuer_key, 
+            sdga.isin AS ISIN,
+            sdga.issuer_name AS "IssuerName",
+            sdga.ticker AS "Ticker",
+            sdga.country_of_incorporation AS "CountryOfIncorporation",
+            sdga.ESG_RATING_INDUSTRY AS "ESGRatingIndustry",
+            sdga.SDG_SOL_ENERGY_PERCENT_COMB_CONT AS "SDGSolEnergyPercentCombCont",
+            sdga.SDG_SOL_CLIMATE_PERCENT_COMB_CONT AS "SDGSolClimatePercentCombCont",
+            sdga.SDG_SOL_MAT_USE_PERCENT_COMB_CONT AS "SDGSolMatUsePercentCombCont",
+            sdga.SDG_SOL_TERR_ECO_PERCENT_COMB_CONT AS "SDGSolTerrEcoPercentCombCont",
+            sdga.SDG_SOL_WATER_PERCENT_COMB_CONT AS "SDGSolWaterPercentCombCont",
+            sdga.SDG_SOL_EDU_PERCENT_COMB_CONT AS "SDGSolEduPercentCombCont",
+            sdga.SDG_SOL_POVERTY_PERCENT_COMB_CONT AS "SDGSolPovertyPercentCombCont",
+            sdga.SDG_SOL_HUNGER_PERCENT_COMB_CONT AS "SDGSolHungerPercentCombCont",
+            sdga.SDG_SOL_MARINE_PERCENT_COMB_CONT AS "SDGSolMarinePercentCombCont",
+            sdga.SDG_SOL_AGRI_PERCENT_COMB_CONT AS "SDGSolAgriPercentCombCont",
+            sdga.SDG_SOL_ENERGY_PROD_COMB_CONT AS "SDGSolEnergyProdCombCont",
+            sdga.SDG_SOL_CLIMATE_PROD_COMB_CONT AS "SDGSolClimateProdCombCont",
+            sdga.SDG_SOL_MAT_USE_PROD_COMB_CONT AS "SDGSolMatUseProdCombCont",
+            sdga.SDG_SOL_TERR_ECO_PROD_COMB_CONT AS "SDGSolTerrEcoProdCombCont",
+            sdga.SDG_SOL_WATER_PROD_COMB_CONT AS "SDGSolWaterProdCombCont",
+            sdga.SDG_SOL_EDU_PROD_COMB_CONT AS "SDGSolEduProdCombCont",
+            sdga.SDG_SOL_POVERTY_PROD_COMB_CONT AS "SDGSolPovertyProdCombCont",
+            sdga.SDG_SOL_HUNGER_PROD_COMB_CONT AS "SDGSolHungerProdCombCont",
+            sdga.SDG_SOL_MARINE_PROD_COMB_CONT AS "SDGSolMarineProdCombCont",
+            sdga.SDG_SOL_AGRI_PROD_COMB_CONT AS "SDGSolAgriProdCombCont",
+            sdgaimpact.CLIMATE_GHG_REDUCTION_TARGETS AS "ClimateGHGReductionTargets",
+            sdgaimpact.BROWN_EXP_TOTAL_CAP_EX_SHARE_PERCENT AS "BrownExpTotalCapExSharePercent",
+            sdgaimpact.GREEN_EXP_TOTAL_CAP_EX_SHARE_PERCENT AS "GreenExpTotalCapExSharePercent"
+        FROM tcw_core_qa.esg_iss.fact_esg_issuer_sdga sdga
+        LEFT JOIN tcw_core_qa.esg_iss.fact_esg_issuer_sdg_impact_rating sdgaimpact
+            ON sdga.ISSUER_KEY = sdgaimpact.ISSUER_KEY
+            AND sdga.IS_CURRENT = sdgaimpact.IS_CURRENT
+        --AND sdga.LAST_AS_OF_DATE = sdgaimpact.LAST_AS_OF_DATE
+        --   WHERE sdga.last_as_of_date = '10/04/2023'
+        WHERE sdga.is_current = 1
+        ORDER BY sdga.issuer_name
+        """
+        self.datasource.load(query=query)
         self.transform_df()
 
     def transform_df(self) -> None:
         """
-        - merge sdg and sdga data into one DataFrame
+        - replace Not Collected with nan
         - change datatype to float for specified columns
         """
-        df_ = self.sdg_datasource.datasource.df.merge(
-            self.sdga_datasource.datasource.df,
-            left_on="ISIN",
-            right_on="ISIN",
-            how="outer",
-        )
-        df_["GreenExpTotalCapExSharePercent"] = df_[
+        self.datasource.df = self.datasource.df.replace("Not Collected", np.nan)
+
+        self.datasource.df["GreenExpTotalCapExSharePercent"] = self.datasource.df[
             "GreenExpTotalCapExSharePercent"
         ].astype(float)
-        df_["SDGSolClimatePercentCombCont"] = df_[
+        self.datasource.df["SDGSolClimatePercentCombCont"] = self.datasource.df[
             "SDGSolClimatePercentCombCont"
         ].astype(float)
 
-        df_["issuerID"] = df_["issuerID_x"].fillna(df_["issuerID_y"])
-        df_ = df_.drop(["issuerID_x", "issuerID_y"], axis=1)
-
-        self.df_ = df_
+        raise NotImplementedError()
 
     def iter(self) -> None:
         """
@@ -84,48 +116,4 @@ class SDGDataSource(object):
         DataFrame
             df
         """
-        return self.df_
-
-
-class SDGData(ds.DataSources):
-    """
-    Wrapper to load SDG and SDGA data
-
-    Parameters
-    ----------
-    params: dict
-        datasource specific parameters including datasource
-    """
-
-    def __init__(self, params: dict, **kwargs) -> None:
-        super().__init__(params, **kwargs)
-
-    def load(self) -> None:
-        """
-        load data and transform dataframe
-        """
-        from_table = f"""{self.database}.{self.schema}."{self.table_name}" """
-        query = f"""
-        SELECT * 
-        FROM {from_table}
-        """
-        self.datasource.load(query=query)
-        self.transform_df()
-
-    def transform_df(self) -> None:
-        """
-        - drop not needed columns
-        - replace 'Not Collected' by nan for every column
-        """
-        self.datasource.df = self.datasource.df.drop(
-            [
-                "IssuerName",
-                "Ticker",
-                "CountryOfIncorporation",
-                "IssuerLEI",
-                "ESGRatingParentEntityName",
-                "ESGRatingParentEntityID",
-            ],
-            axis=1,
-        )
-        self.datasource.df = self.datasource.df.replace("Not Collected", np.nan)
+        return self.datasource.df
