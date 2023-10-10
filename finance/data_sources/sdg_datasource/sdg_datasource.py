77b1188fafc00334139e334b99245536277fc1d3
Bastian <Tim.Bastian@tcw.com>
2023-10-10 08:19:53 -0700

    new query

--- finance/data_sources/sdg_datasource/sdg_datasource.py
+++ finance/data_sources/sdg_datasource/sdg_datasource.py
@@ -34,47 +34,66 @@ class SDGDataSource(ds.DataSources):
         """
         logging.log("Loading SDG Data")
         query = f"""
-        SELECT 
-            sdga.issuer_id, 
-            sdga.issuer_key, 
-            sdga.isin AS ISIN,
-            sdga.issuer_name AS "IssuerName",
-            sdga.ticker AS "Ticker",
-            sdga.country_of_incorporation AS "CountryOfIncorporation",
-            sdga.esg_rating_industry AS "ESGRatingIndustry",
-            sdga.sdg_sol_energy_percent_comb_cont AS "SDGSolEnergyPercentCombCont",
-            sdga.sdg_sol_climate_percent_comb_cont AS "SDGSolClimatePercentCombCont",
-            sdga.sdg_sol_mat_use_percent_comb_cont AS "SDGSolMatUsePercentCombCont",
-            sdga.sdg_sol_terr_eco_percent_comb_cont AS "SDGSolTerrEcoPercentCombCont",
-            sdga.sdg_sol_water_percent_comb_cont AS "SDGSolWaterPercentCombCont",
-            sdga.sdg_sol_edu_percent_comb_cont AS "SDGSolEduPercentCombCont",
-            sdga.sdg_sol_poverty_percent_comb_cont AS "SDGSolPovertyPercentCombCont",
-            sdga.sdg_sol_hunger_percent_comb_cont AS "SDGSolHungerPercentCombCont",
-            sdga.sdg_sol_marine_percent_comb_cont AS "SDGSolMarinePercentCombCont",
-            sdga.sdg_sol_agri_percent_comb_cont AS "SDGSolAgriPercentCombCont",
-            sdga.sdg_sol_sus_build_percent_comb_cont "SDGSolSusBuildPercentCombCont",
-            sdga.sdg_sol_energy_prod_comb_cont AS "SDGSolEnergyProdCombCont",
-            sdga.sdg_sol_climate_prod_comb_cont AS "SDGSolClimateProdCombCont",
-            sdga.sdg_sol_mat_use_prod_comb_cont AS "SDGSolMatUseProdCombCont",
-            sdga.sdg_sol_terr_eco_prod_comb_cont AS "SDGSolTerrEcoProdCombCont",
-            sdga.sdg_sol_water_prod_comb_cont AS "SDGSolWaterProdCombCont",
-            sdga.sdg_sol_edu_prod_comb_cont AS "SDGSolEduProdCombCont",
-            sdga.sdg_sol_poverty_prod_comb_cont AS "SDGSolPovertyProdCombCont",
-            sdga.sdg_sol_hunger_prod_comb_cont AS "SDGSolHungerProdCombCont",
-            sdga.sdg_sol_marine_prod_comb_cont AS "SDGSolMarineProdCombCont",
-            sdga.sdg_sol_agri_prod_comb_cont AS "SDGSolAgriProdCombCont", 
-            sdga.sdg_sol_sus_build_prod_comb_cont "SDGSolSusBuildProdCombCont",
-            sdgaimpact.climate_ghg_reduction_targets AS "ClimateGHGReductionTargets",
-            sdgaimpact.brown_exp_total_cap_ex_share_percent AS "BrownExpTotalCapExSharePercent",
-            sdgaimpact.green_exp_total_cap_ex_share_percent AS "GreenExpTotalCapExSharePercent"
-        FROM tcw_core_qa.esg_iss.fact_esg_issuer_sdga sdga
-        LEFT JOIN tcw_core_qa.esg_iss.fact_esg_issuer_sdg_impact_rating sdgaimpact
-            ON sdga.issuer_key = sdgaimpact.issuer_key
-            AND sdga.is_current = sdgaimpact.is_current
-        --AND sdga.last_as_of_date = sdgaimpact.last_as_of_date
-        --   WHERE sdga.last_as_of_date = '10/04/2023'
-        WHERE sdga.is_current = 1
-        ORDER BY sdga.issuer_name
+        WITH date_table_sdga AS (
+            SELECT issuer_key, MAX(last_as_of_date) AS max_load_date
+            FROM tcw_core_qa.esg_iss.fact_esg_issuer_sdga
+            WHERE last_as_of_date <= CAST('09/30/2023' AS DATE)
+            GROUP BY issuer_key
+        ),
+        date_table_sdgaimpact AS (
+            SELECT issuer_key, MAX(last_as_of_date) AS max_load_date
+            FROM tcw_core_qa.esg_iss.fact_esg_issuer_sdg_impact_rating
+            WHERE last_as_of_date <= CAST('09/30/2023' AS DATE)
+            GROUP BY issuer_key
+        ), 
+        full_query AS (
+            SELECT 
+                sdga.issuer_id, 
+                sdga.issuer_key, 
+                sdga.isin AS ISIN,
+                sdga.issuer_name AS "IssuerName",
+                sdga.ticker AS "Ticker",
+                sdga.country_of_incorporation AS "CountryOfIncorporation",
+                sdga.esg_rating_industry AS "ESGRatingIndustry",
+                sdga.sdg_sol_energy_percent_comb_cont AS "SDGSolEnergyPercentCombCont",
+                sdga.sdg_sol_climate_percent_comb_cont AS "SDGSolClimatePercentCombCont",
+                sdga.sdg_sol_mat_use_percent_comb_cont AS "SDGSolMatUsePercentCombCont",
+                sdga.sdg_sol_terr_eco_percent_comb_cont AS "SDGSolTerrEcoPercentCombCont",
+                sdga.sdg_sol_water_percent_comb_cont AS "SDGSolWaterPercentCombCont",
+                sdga.sdg_sol_edu_percent_comb_cont AS "SDGSolEduPercentCombCont",
+                sdga.sdg_sol_poverty_percent_comb_cont AS "SDGSolPovertyPercentCombCont",
+                sdga.sdg_sol_hunger_percent_comb_cont AS "SDGSolHungerPercentCombCont",
+                sdga.sdg_sol_marine_percent_comb_cont AS "SDGSolMarinePercentCombCont",
+                sdga.sdg_sol_agri_percent_comb_cont AS "SDGSolAgriPercentCombCont",
+                sdga.sdg_sol_sus_build_percent_comb_cont "SDGSolSusBuildPercentCombCont",
+                sdga.sdg_sol_energy_prod_comb_cont AS "SDGSolEnergyProdCombCont",
+                sdga.sdg_sol_climate_prod_comb_cont AS "SDGSolClimateProdCombCont",
+                sdga.sdg_sol_mat_use_prod_comb_cont AS "SDGSolMatUseProdCombCont",
+                sdga.sdg_sol_terr_eco_prod_comb_cont AS "SDGSolTerrEcoProdCombCont",
+                sdga.sdg_sol_water_prod_comb_cont AS "SDGSolWaterProdCombCont",
+                sdga.sdg_sol_edu_prod_comb_cont AS "SDGSolEduProdCombCont",
+                sdga.sdg_sol_poverty_prod_comb_cont AS "SDGSolPovertyProdCombCont",
+                sdga.sdg_sol_hunger_prod_comb_cont AS "SDGSolHungerProdCombCont",
+                sdga.sdg_sol_marine_prod_comb_cont AS "SDGSolMarineProdCombCont",
+                sdga.sdg_sol_agri_prod_comb_cont AS "SDGSolAgriProdCombCont", 
+                sdga.sdg_sol_sus_build_prod_comb_cont "SDGSolSusBuildProdCombCont",
+                sdgaimpact.climate_ghg_reduction_targets AS "ClimateGHGReductionTargets",
+                sdgaimpact.brown_exp_total_cap_ex_share_percent AS "BrownExpTotalCapExSharePercent",
+                sdgaimpact.green_exp_total_cap_ex_share_percent AS "GreenExpTotalCapExSharePercent"
+            FROM tcw_core_qa.esg_iss.fact_esg_issuer_sdga sdga
+            LEFT JOIN tcw_core_qa.esg_iss.fact_esg_issuer_sdg_impact_rating sdgaimpact
+                ON sdga.issuer_key = sdgaimpact.issuer_key
+            JOIN date_table_sdga
+                ON date_table_sdga.issuer_key = sdga.issuer_key
+                AND date_table_sdga.max_load_date = sdga.last_as_of_date
+            JOIN date_table_sdgaimpact
+                ON date_table_sdgaimpact.issuer_key = sdgaimpact.issuer_key
+                AND date_table_sdgaimpact.max_load_date = sdgaimpact.last_as_of_date
+        )
+
+        SELECT DISTINCT(issuer_id), *
+        FROM full_query
+        ORDER BY issuer_key
         """
         self.datasource.load(query=query)
         self.transform_df()
