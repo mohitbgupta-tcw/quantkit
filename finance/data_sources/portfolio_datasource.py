760e2ec99725aa8905157a119e4787da6291b5ad
Bastian <Tim.Bastian@tcw.com>
2023-12-26 18:16:35 -0800

    iter holdings

--- finance/data_sources/portfolio_datasource.py
+++ finance/data_sources/portfolio_datasource.py
@@ -455,9 +455,11 @@ class PortfolioDataSource(ds.DataSources):
             dictionary of msci information
         """
         logging.log("Iterate Holdings")
-        for index, row in self.df.iterrows():
+
+        grouped = self.df.groupby("ISIN")
+        for isin, group in grouped:
             sec_info = (
-                row[
+                group.tail(1)[
                     [
                         "ESG Collateral Type",
                         "ISIN",
@@ -484,9 +486,6 @@ class PortfolioDataSource(ds.DataSources):
                 .to_dict()
             )
 
-            pf = row["Portfolio"]  # portfolio id
-            isin = sec_info["ISIN"]  # security isin
-
             # get MSCI information
             msci_information = self.create_msci_information(sec_info, msci_dict)
 
@@ -497,31 +496,23 @@ class PortfolioDataSource(ds.DataSources):
             # create company object
             issuer = sec_info["Issuer ISIN"]
             self.create_parent_store(
-                sector_level_2=row["Sector Level 2"],
+                sector_level_2=sec_info["Sector Level 2"],
                 issuer_isin=issuer,
                 msci_information=msci_information,
                 security_store=security_store,
             )
 
-            # attach security object, portfolio weight, OAS to portfolio
-            holding_measures = row[
-                ["Portfolio_Weight", "Base Mkt Val", "OAS"]
-            ].to_dict()
-            if isin in self.portfolios[pf].holdings:
-                self.portfolios[pf].holdings[isin]["holding_measures"][
-                    "Portfolio_Weight"
-                ] += holding_measures["Portfolio_Weight"]
-                self.portfolios[pf].holdings[isin]["holding_measures"][
-                    "Base Mkt Val"
-                ] += holding_measures["Base Mkt Val"]
-            else:
-                self.portfolios[pf].holdings[isin] = {
-                    "object": security_store,
-                    "holding_measures": holding_measures,
+            # attach securities to portfolios
+            portfolio_rows = group.groupby("Portfolio").agg(
+                {
+                    "ISIN": "last",
+                    "As Of Date": "last",
+                    "Portfolio_Weight": "sum",
+                    "Base Mkt Val": "sum",
+                    "OAS": "last",
                 }
-
-            # attach portfolio to security
-            security_store.portfolio_store[pf] = self.portfolios[pf]
+            )
+            self.attach_securities(portfolio_rows, isin)
 
     def create_security_store(
         self, security_isin: str, security_information: dict
@@ -536,11 +527,10 @@ class PortfolioDataSource(ds.DataSources):
         security_information: dict
             dictionary of information about the security
         """
-        if not security_isin in self.securities:
-            security_store = securitites.SecurityStore(
-                isin=security_isin, information=security_information
-            )
-            self.securities[security_isin] = security_store
+        security_store = securitites.SecurityStore(
+            isin=security_isin, information=security_information
+        )
+        self.securities[security_isin] = security_store
 
     def create_parent_store(
         self,
@@ -627,6 +617,30 @@ class PortfolioDataSource(ds.DataSources):
             issuer_dict["ISSUER_ISIN"] = security_information["ISIN"]
             issuer_dict["ISSUER_NAME"] = security_information["Security_Name"]
         return issuer_dict
+
+    def attach_securities(self, portfolio_rows: pd.DataFrame, isin: str) -> None:
+        """
+        Attach security object, portfolio weight, OAS to portfolios
+
+        Parameters
+        ----------
+        portfolio_rows: pd.DataFrame
+            DataFrane of portfolios security is held in
+        isin: str
+            security isin
+        """
+        security_store = self.securities[isin]
+        for pf, row in portfolio_rows.iterrows():
+            holding_measures = row[
+                ["As Of Date", "Portfolio_Weight", "Base Mkt Val", "OAS"]
+            ].to_dict()
+            self.portfolios[pf].holdings[isin] = {
+                "object": security_store,
+                "holding_measures": holding_measures,
+            }
+
+        # attach portfolio to security
+        security_store.portfolio_store[pf] = self.portfolios[pf]
 
     @property
     def all_msci_ids(self):
