a5e0596556d62eba4755fa83de2374a3c27a9d18
mohitbgupta-tcw <mohit.gupta@tcw.com>
2024-06-05 18:59:51 -0700

    Update q_alpha.py

--- code/q_alpha/q_alpha.py
+++ code/q_alpha/q_alpha.py
@@ -1,4 +1,12 @@
 import pandas as pd
 import numpy as np
-import quantkit.backtester.runner_backtester as runner
+#import quantkit.backtester.runner_backtester as runner
 import warnings
+# add quantkit to path
+import sys
+from pathlib import Path
+
+d = Path().resolve().parent
+print (d)
+print ('-------------------1111111111111111111------------------------')
+
