a239604d732bdd49c38a636c4eaed75b200c62fd
mohitbgupta-tcw <mohit.gupta@tcw.com>
2024-06-06 09:11:16 -0700

    Update runner_backtester.py

--- backtester/runner_backtester.py
+++ backtester/runner_backtester.py
@@ -90,20 +90,20 @@ class Runner(loader.Runner):
         """
         iterate over DataFrames and create connected objects
         """
-        self.iter_parent_issuers()
+        #self.iter_parent_issuers()
         self.iter_portfolios()
-        self.iter_msci()
-        self.iter_prices()
-        self.iter_fundamentals()
-        self.iter_marketmuliples()
-        self.iter_holdings()
-        self.iter_securities()
-        self.iter_cash()
-        self.iter_companies()
-        self.iter_sovereigns()
-        self.iter_securitized()
-        self.iter_muni()
-        self.init_strategies()
+        #self.iter_msci()
+        #self.iter_prices()
+        #self.iter_fundamentals()
+        #self.iter_marketmuliples()
+        #self.iter_holdings()
+        #self.iter_securities()
+        #self.iter_cash()
+        #self.iter_companies()
+        #self.iter_sovereigns()
+        #self.iter_securitized()
+        #self.iter_muni()
+        #self.init_strategies()
 
     def iter_parent_issuers(self) -> None:
         """
