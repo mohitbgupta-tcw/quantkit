ccae80f97b72f11869247179525b7ce245335211
mohitbgupta-tcw <mohit.gupta@tcw.com>
2024-06-06 08:47:41 -0700

    Update q_alpha.py

--- code/q_alpha/q_alpha.py
+++ code/q_alpha/q_alpha.py
@@ -12,7 +12,7 @@ sys.path.insert(0, str(d)+'/wd')
 import quantkit.backtester.runner_backtester as runner
 
 warnings.filterwarnings("ignore")
-local_configs = str(Path(__file__).resolve().parent.parent)+'/code/q_alpha/configs_q_alpha.json'
+local_configs = str(Path(__file__).resolve().parent.parent)+'/q_alpha/configs_q_alpha.json'
 print(local_configs)
 r = runner.Runner()
 r.init(local_configs=local_configs)
