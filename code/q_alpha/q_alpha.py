4e32692f43e475f86eb9fae3871f570bfac1dc25
mohitbgupta-tcw <mohit.gupta@tcw.com>
2024-06-05 19:10:21 -0700

    Update q_alpha.py

--- code/q_alpha/q_alpha.py
+++ code/q_alpha/q_alpha.py
@@ -7,6 +7,7 @@ import sys
 from pathlib import Path
 
 d = Path().resolve().parent
+sys.path.insert(0, str(d)+'wd/quantkit')
 print (d)
 print ('-------------------1111111111111111111------------------------')
 
