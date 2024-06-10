eec0a776ce77b82c43d682cf7e174e084d713552
mohitbgupta-tcw <mohit.gupta@tcw.com>
2024-06-10 09:46:04 -0700

    Update risk_framework.py

--- code/sustainable/risk_framework.py
+++ code/sustainable/risk_framework.py
@@ -1,1 +1,18 @@
+import pandas as pd
+import numpy as np
+import warnings
+# add quantkit to path
+import sys
+from pathlib import Path
 
+d = Path().resolve().parent
+sys.path.insert(0, str(d)+'/wd')
+#print (str(d)+'/wd')
+
+#import quantkit.backtester.runner_backtester as runner
+import quantkit.handyman.risk_framework as risk_framework
+
+local_configs = str(Path(__file__).resolve().parent.parent.parent)+'/q_alpha/configs_q_alpha.json' "C:\\Users\\bastit\\OneDrive - The TCW Group Inc\\Documents\\quantkit\\configs\\configs.json"
+
+#df_detailed = risk_framework.risk_framework(local_configs=local_configs)
+print(local_configs)
