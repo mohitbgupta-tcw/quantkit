9d9df2a77732319f7de0d0e297f0b65c40187b52
mohitbgupta-tcw <mohit.gupta@tcw.com>
2024-06-10 10:03:42 -0700

    Update risk_framework.py

--- code/sustainable/risk_framework.py
+++ code/sustainable/risk_framework.py
@@ -7,7 +7,7 @@ from pathlib import Path
 
 d = Path().resolve().parent
 sys.path.insert(0, str(d)+'/wd')
-#print (str(d)+'/wd')
+print (str(d)+'/wd')
 
 #import quantkit.backtester.runner_backtester as runner
 import quantkit.handyman.risk_framework as risk_framework
@@ -15,4 +15,6 @@ import quantkit.handyman.risk_framework as risk_framework
 local_configs = str(Path(__file__).resolve().parent.parent.parent)+'/configs/configs.json' 
 
 #df_detailed = risk_framework.risk_framework(local_configs=local_configs)
+print ('1111111111111111111111111111111111111')
 print(local_configs)
+print ('3333333333333333333333333333333333333333')
