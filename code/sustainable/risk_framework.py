74316024b42bbd009491c98412d426ce80acf016
mohitbgupta-tcw <mohit.gupta@tcw.com>
2024-06-14 15:11:12 -0700

    Update risk_framework.py

--- code/sustainable/risk_framework.py
+++ code/sustainable/risk_framework.py
@@ -17,4 +17,32 @@ local_configs = str(Path(__file__).resolve().parent.parent.parent)+'/configs/con
 df_detailed = risk_framework.risk_framework(local_configs=local_configs)
 
 #print(local_configs)
+
+import quantkit.utils.snowflake_utils as snowflake_utils
+
+local_configs = "C:\\Users\\bastit\\OneDrive - The TCW Group Inc\\Documents\\quantkit\\configs\\configs.json"
+
+df_detailed['Portfolio ISIN'] = df_detailed['Portfolio ISIN'].astype(str)
+df_detailed['Ticker'] = df_detailed['Ticker'].astype(str)
+df_detailed['As Of Date'] = df_detailed['As Of Date'].astype(str)
+df_detailed['Portfolio Weight'] = df_detailed['Portfolio Weight'].astype(float)
+df_detailed['Alcohol'] = df_detailed['Alcohol'].astype(float)
+df_detailed['Tobacco'] = df_detailed['Tobacco'].astype(float)
+df_detailed['Oil_Gas'] = df_detailed['Oil_Gas'].astype(float)
+df_detailed['Gambling'] = df_detailed['Gambling'].astype(float)
+df_detailed['Weapons_Firearms'] = df_detailed['Weapons_Firearms'].astype(float)
+df_detailed['Thermal_Coal_Mining'] = df_detailed['Thermal_Coal_Mining'].astype(float)
+df_detailed['Thermal_Coal_Power_Gen'] = df_detailed['Thermal_Coal_Power_Gen'].astype(float)
+df_detailed['Adult_Entertainment'] = df_detailed['Adult_Entertainment'].astype(float)
+df_detailed['Controversial_Weapons'] = df_detailed['Controversial_Weapons'].astype(str)
+df_detailed['UN_Alignement'] = df_detailed['UN_Alignement'].astype(str)
+
+snowflake_utils.write_to_snowflake(
+df_detailed,
+database="SANDBOX_ESG",
+schema="ESG_SCORES_THEMES",
+table_name="Sustainability_Framework_Detailed",
+local_configs=local_configs
+)
+
 print ('---------Success---------')
